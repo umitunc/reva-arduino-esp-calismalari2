e94b13a01292f6984e0a0d74b37fba5949b69cd4
Reva Tunç <umitreva@icloud.com>
2026-06-10 17:28:47 +0300

    feat: initialize motor controller setup for AFMotor library in main.cpp

--- reva_car/src/main.cpp
+++ reva_car/src/main.cpp
@@ -7,8 +7,7 @@ AF_DCMotor motor2(2);
 
 void setup() {
   Serial.begin(9600);           
-  Serial.println("Motor Testi Basliyor!");
-
+  
   // Motorların hızını ayarlıyoruz (0 ile 255 arasında bir değer)
   motor1.setSpeed(200); 
   motor2.setSpeed(200); 
@@ -19,28 +18,7 @@ void setup() {
 }
 
 void loop() {
-  Serial.println("İleri Donus");
-  // Motorları ileri yönde döndür
-  motor1.run(FORWARD);
-  motor2.run(FORWARD);
-  delay(3000); // 3 saniye bekle
-
-  Serial.println("Motorlar Duruyor");
-  // Motorları durdur
-  motor1.run(RELEASE);
-  motor2.run(RELEASE);
-  delay(1000); // 1 saniye bekle
-
-  Serial.println("Geri Donus");
-  // Motorları geri yönde döndür
-  motor1.run(BACKWARD);
-  motor2.run(BACKWARD);
-  delay(3000); // 3 saniye bekle
-
-  Serial.println("Motorlar Duruyor");
-  // Motorları tekrar durdur
-  motor1.run(RELEASE);
-  motor2.run(RELEASE);
-  delay(2000); // 2 saniye bekle
+  // Kontrol kodları buraya eklenebilir
 }
+
 
